7fd5afe9f7663bf1a7004dd2a5464a41b55a27f5
Cahya Wirawan <cahya.wirawan@gmail.com>
2017-07-09 20:08:19 +0200

    Changed the gradientdescent with adam optimizer

--- mnist/mnist_0_3.py
+++ mnist/mnist_0_3.py
@@ -62,7 +62,8 @@ correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # training, learning rate = 0.005
-train_step = tf.train.GradientDescentOptimizer(lr).minimize(cross_entropy)
+## train_step = tf.train.GradientDescentOptimizer(lr).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
 
 
 sess = tf.InteractiveSession()
